fdfab3f0a5f405fd5e81782f6229968d7dd73ed3
Алексей Козополянский <leshkageek@gmail.com>
2021-03-02 13:02:16 +0300

    Updates

---
+++ precache-manifest.8bb46c0b6d728d0b0ced33925c5754da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a61028c3e17b08fd48fc0007900ed81",
+    "url": "/Alex-Kozopolianski/index.html"
+  },
+  {
+    "revision": "caca18a1473c564a0c3e",
+    "url": "/Alex-Kozopolianski/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "33393877306b9bb366d4",
+    "url": "/Alex-Kozopolianski/static/css/main.2d28ed8e.chunk.css"
+  },
+  {
+    "revision": "caca18a1473c564a0c3e",
+    "url": "/Alex-Kozopolianski/static/js/2.a3c7d2cb.chunk.js"
+  },
+  {
+    "revision": "33393877306b9bb366d4",
+    "url": "/Alex-Kozopolianski/static/js/main.3d6c84e0.chunk.js"
+  },
+  {
+    "revision": "e2a930e300275f57fc8f",
+    "url": "/Alex-Kozopolianski/static/js/runtime-main.6b5586d8.js"
+  },
+  {
+    "revision": "2590400441ea2b70577cf33441e39004",
+    "url": "/Alex-Kozopolianski/static/media/banner.25904004.jpg"
+  }
+]);
